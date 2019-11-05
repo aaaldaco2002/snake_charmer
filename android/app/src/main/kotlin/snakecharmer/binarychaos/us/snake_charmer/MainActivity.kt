98280a722ca6700be47288977dd56f05d8bef0d5
Angel <aaaldaco2002@gmail.com>
2019-11-05 07:57:59 -0800

    Initial commit

---
+++ android/app/src/main/kotlin/snakecharmer/binarychaos/us/snake_charmer/MainActivity.kt
@@ -0,0 +1,13 @@
+package snakecharmer.binarychaos.us.snake_charmer
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
