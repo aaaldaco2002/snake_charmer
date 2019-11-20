4468bd3674125f2e32b370b6b45c46ceb561bd5b
Angel <aaaldaco2002@gmail.com>
2019-11-20 08:16:59 -0800

    added fonts and finised most of the information page

--- lib/detail_view.dart
+++ lib/detail_view.dart
@@ -14,7 +14,13 @@ class _DetailViewState extends State<DetailView> {
         color: Colors.black,
         child: ListView(
           children: <Widget>[
-            Text('(species title)',
+            Text('Species',
+              style: TextStyle(
+                  color: Colors.white
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text('Python',
                 style: TextStyle(
                   color: Colors.white
                 ),
@@ -24,7 +30,41 @@ class _DetailViewState extends State<DetailView> {
               "assets/python.jpg",
               width: 210.0,
               height: 210.0,
-            )
+            ),
+            Text(
+              'Information',
+              style: TextStyle(
+                  color: Colors.white,
+                fontFamily: 'RobotoMono-Regular',
+
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'Python is a genus of constricting snakes in the Pythonidae family native to the tropics and subtropics of the Eastern Hemisphere. The name Python was proposed by François Marie Daudin in 1803 for non-venomous flecked snakes. Currently, 10 python species are recognized as valid taxa',
+               style: TextStyle (
+               color: Colors.white
+               ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'What to do if bitten',
+              style: TextStyle(
+                  color: Colors.white,
+
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'The good news is that Ball Python bites are generally harmless and otherwise painless. It’s more panic-inducing than anything else. If the proper steps are taken, the worst that will happen is a few drops of blood and a band-aid.',
+              style: TextStyle(
+                  color: Colors.white
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+
+
             //Image.asset(Dangerlevel)
 
           ],
