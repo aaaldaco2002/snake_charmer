34ee74a4eff7af3a7e04a20300d2a01739ffc9dc
Aaron Whitfield <aawhitfield@hotmail.com>
2019-12-12 08:16:30 -0800

    finished creating a family string with all of the snake families for the list title and another list with the common snake names for the list's subtitle. Adjusted text size accordingly.

--- lib/family_filter.dart
+++ lib/family_filter.dart
@@ -7,6 +7,15 @@ class FamilyFilter extends StatefulWidget {
 }
 
 class _FamilyFilterState extends State<FamilyFilter> {
+
+  List<String> snakeFamiliesTitle = [
+   'Acrochordidae', 'Aniliidae', 'Anomalepidae', 'Anomochilidae', 'Atractspididae', 'Boidae', 'Bolyeridae', 'Colubridae', 'Cylindrphiidae', 'Elapidae', 'Leptotyphlopidae', 'Loxocemidae', 'Pythonidae', 'Tropidophiidae', 'Typhlopidae', 'Uropeltidae', 'Viperidae', 'Xenopeltidae',
+  ];
+
+  List<String> snakeFamiliesSubtitle = [
+    '(File Snakes)', '(Coral Pipe Snakes)', '(Dawn Blind Snakes)', '(Dwarf Pipe Snakes)', '(Mole Vipers)', '(Boas)', '(Round Island Boas)', '(Colubrids)', '(Asian Pipe Snakes)', '(Cobras, Coral Snakes, Mambas, Kraits)', '(Slender Blind Snakes)', '(Mexican Burrowing Snakes)', '(Pythons)', '(Dwarf Boas)', '(Blind Snakes)', '(Shield/Short-Tailed Snakes)', '(Vipers, Pitvipers, Rattlesnakes)', '(Sunbeam Snake)',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,41 +35,36 @@ class _FamilyFilterState extends State<FamilyFilter> {
             color: Colors.black,
             child: ListView.builder(
                 shrinkWrap: true,
-                itemCount: 20,
+                itemCount: 18,
                 itemBuilder: (BuildContext context, int index) {
                   return GestureDetector(
-
-                      child: Row(
-                          children: <Widget>[
-                            Container(
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  border: Border.all(
-                                    color: Colors.white,
-                                    width: 1.0,
-                                  )),
-                              child: ListTile(
-                                trailing: Icon(
-                                  Icons.add_circle_outline,
-                                  color: Colors.white,
-                                ),
-                                title: Text('Acrochordidae (File Snakes)',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.0,
-                                    )),
-                              ),
-                            )
-                          ]
-                      ),
-                  );
-                }
-            )
-        ));
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                        color: Colors.black,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        )),
+                        child: ListTile(
+                          trailing: Icon(
+                          Icons.add_circle_outline,
+                          color: Colors.white,
+                          ),
+                          subtitle: Text(snakeFamiliesSubtitle[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          ),
+                          ),
+                          title: Text(snakeFamiliesTitle[index],
+                            style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          )),
+                    ),
+                  ));
+                })));
   }
 }
